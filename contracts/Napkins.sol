412d75a19306f40834bb0ad3acdf5ae8592eff7e
Josep Servat <josep.servat@guidesmiths.com>
2018-03-03 17:42:32 +0100

    first contract

---
+++ contracts/Napkins.sol
@@ -0,0 +1,60 @@
+// Ethereum Name Registrar as it should be!
+//
+// Written by Alexandre Naverniouk
+// twitter @AlexNa
+
+
+contract Napker {
+
+    uint constant MAX_PROLONG = 2000000; // Maximum number of blocks to prolong the ownership. About one year.
+    
+    uint public n_napkers = 0;      // total number of registered domains
+    uint public root_napkers = 0;    // name of the first domain in the linked list
+    address napker = 0; // owner
+    
+    struct Id {                     // Id record. Double linked list. Allows to delete ID
+        uint value;
+        uint next_id;
+        uint prev_id;
+    }
+    
+    struct Napker {                 // Domain record. Linked list. 
+        address owner;              
+        uint asset;    
+        uint root_id;           
+        mapping (uint => Id) ids;   // Map of the ID's
+    }
+    
+    mapping (uint => Napker) napkers; // Map of the domains
+    
+    function Napkin()
+    {
+        napker = msg.sender;
+    }
+    
+    
+    function getId( uint napker, uint id ) constant returns (uint v, uint next_id, uint prev_id )
+    {
+        Id i = napkers[napker].ids[id]; 
+    
+        v = i.value;
+        next_id = i.next_id;
+        prev_id = i.prev_id;
+    }
+    
+    function getNapker( uint domain ) constant returns 
+        (address owner, uint asset, uint root_id )
+    {
+        Napker d = napkers[ napker ];
+        
+        owner = d.owner;
+        asset = d.asset;
+        root_id = d.root_id;    
+    }
+    
+  
+    if( msg.value > 0 ) // If transaction has any money...
+    {
+        if( !msg.sender.send( msg.value ) ) throw; // ... it is a mistake, so send it back
+    }
+}
